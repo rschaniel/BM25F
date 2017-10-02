651ec74045a1b9a2a3c9fadb6a8cbc41e7b84c9e
Ronnie Schaniel <ronnieschaniel@gmail.com>
2017-10-02 20:54:57 +0200

    pipy prep

--- setup.py
+++ setup.py
@@ -2,6 +2,17 @@
 # coding: utf-8
 
 from distutils.core import setup
-setup(name='BM25F',
-      version='0.0.2',
-      packages=['BM25F'])
+
+
+setup(
+  name = 'BM25F',
+  packages = ['BM25F'],
+  version = '0.0.2',
+  description = 'BM25F library',
+  author = 'Ronnie Schaniel',
+  author_email = 'ronnieschaniel@gmail.com',
+  url = 'https://github.com/rschaniel/BM25F',
+  download_url = 'https://github.com/rschaniel/BM25F/archive/0.0.2.tar.gz',
+  keywords = ['BM25F', 'IR'],
+  classifiers = [],
+)
